d35976d8ef3454da422e9c647773deee974383b2
RasulovaB <55116283+RasulovaB@users.noreply.github.com>
2020-07-19 14:01:44 -0400

    Create recursion.js

---
+++ recursion.js
@@ -0,0 +1,21 @@
+//Recursion
+function isEven(number){
+  if(number === 0){
+    return true;
+  }
+  else if (number === 1){
+    return false;
+  }
+  else if(number<0){
+    return '??';
+  } else{
+    return isEven(number-2);
+  }
+}
+
+console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-1));
+// → ??
